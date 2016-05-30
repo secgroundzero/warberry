9aab67ce0018963eb63a05687b9315e11def26a3
Ryan Jarvis <cabalist@gmail.com>
2016-05-30 13:03:47 -0700

    more pep8

--- resources/network_scanners.py
+++ resources/network_scanners.py
@@ -19,7 +19,7 @@ import os
 import nmap
 
 
-class bcolors:
+class bcolors(object):
     HEADER = '\033[95m'
     OKBLUE = '\033[34m'
     OKGREEN = '\033[32m'
@@ -30,7 +30,7 @@ class bcolors:
     TITLE = '\033[96m'
 
 
-def scanner_targetted(CIDR):
+def scanner_targetted(cidr):
     print(" ")
     print(bcolors.OKGREEN + "      [ TARGETTED SERVICES NETWORK SCANNER MODULE ]\n" + bcolors.ENDC)
 
@@ -73,12 +73,12 @@ def scanner_targetted(CIDR):
     if os.path.isfile('/home/pi/WarBerry/Results/snmp'):
         print(bcolors.WARNING + "[!]" + bcolors.ENDC + " SNMP Results File Exists. New results will be appended ")
 
-    print("\n[*] Beginning Scanning Subnet %s" % CIDR)
+    print("\n[*] Beginning Scanning Subnet %s" % cidr)
     print(" ")
     nm = nmap.PortScanner()
 
     print("[+] Scanning for Windows Hosts...")
-    nm.scan(hosts=CIDR, arguments='-Pn -p445 -T4 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -p445 -T4 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/windows', 'a') as windows:
             print('----------------------------------------------------')
@@ -87,7 +87,7 @@ def scanner_targetted(CIDR):
             windows.write('%s\n' % host)
 
     print("[+] Scanning for FTP...")
-    nm.scan(hosts=CIDR, arguments='-Pn -p21 -T4 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -p21 -T4 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/ftp', 'a') as ftp:
             print('----------------------------------------------------')
@@ -96,7 +96,7 @@ def scanner_targetted(CIDR):
             ftp.write('%s\n' % host)
 
     print("[+] Scanning for MSSQL Databases...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p1433 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p1433 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/mssql', 'a') as mssql:
             print('----------------------------------------------------')
@@ -105,7 +105,7 @@ def scanner_targetted(CIDR):
             mssql.write('%s\n' % host)
 
     print("[+] Scanning for MySQL Databases...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p3306 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p3306 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/mysql', 'a') as mysql:
             print('----------------------------------------------------')
@@ -114,7 +114,7 @@ def scanner_targetted(CIDR):
             mysql.write('%s\n' % host)
 
     print("[+] Scanning for Oracle Databases...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p1521 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p1521 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/oracle', 'a') as oracle:
             print('----------------------------------------------------')
@@ -124,7 +124,7 @@ def scanner_targetted(CIDR):
             oracle.write('%s\n' % host)
 
     print("[+] Scanning for NFS...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p111 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p111 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/nfs', 'a') as nfs:
             print('----------------------------------------------------')
@@ -133,49 +133,49 @@ def scanner_targetted(CIDR):
             nfs.write('%s\n' % host)
 
     print("[+] Scanning for Web Servers...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p80 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p80 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers80', 'a') as webservers80:
             print('----------------------------------------------------')
             print(bcolors.OKGREEN + ('   *** Web Server Found : %s via port 80 ***' % host) + bcolors.ENDC)
             webservers80.write('%s\n' % host)
 
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p443 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p443 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers443', 'a') as webservers443:
             print('----------------------------------------------------')
             print(bcolors.OKGREEN + ('   *** Web Server Found : %s via port 443 ***' % host) + bcolors.ENDC)
             webservers443.write('%s\n' % host)
 
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p8080 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p8080 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers8080', 'a') as webservers8080:
             print('----------------------------------------------------')
             print(bcolors.OKGREEN + ('   *** Web Server Found : %s via port 8080 ***' % host) + bcolors.ENDC)
             webservers8080.write('%s\n' % host)
 
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p4443 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p4443 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers4443', 'a') as webservers4443:
             print('----------------------------------------------------')
             print(bcolors.OKGREEN + ('   *** Web Server Found : %s via port 4443 ***' % host) + bcolors.ENDC)
             webservers4443.write('%s\n' % host)
 
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p8081 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p8081 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers8081', 'a') as webservers8081:
             print('----------------------------------------------------')
             print(bcolors.OKGREEN + ('   *** Web Server Found : %s via port 8081 ***' % host) + bcolors.ENDC)
             webservers8081.write('%s\n' % host)
 
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p8181 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p8181 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers8181', 'a') as webservers8181:
             print('----------------------------------------------------')
             print(bcolors.OKGREEN + ('   *** Web Server Found : %s via port 8181 ***' % host) + bcolors.ENDC)
             webservers8181.write('%s\n' % host)
 
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p9090 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p9090 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/webservers9090', 'a') as webservers9090:
             print('----------------------------------------------------')
@@ -183,7 +183,7 @@ def scanner_targetted(CIDR):
             webservers9090.write('%s\n' % host)
 
     print("[+] Scanning for Printers...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p8611,8612,5222,5223 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p8611,8612,5222,5223 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/printers', 'a') as printers:
             print('----------------------------------------------------')
@@ -191,7 +191,7 @@ def scanner_targetted(CIDR):
             printers.write('%s\n' % host)
 
     print("[+] Scanning for Telnet...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p23 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p23 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/telnet', 'a') as telnet:
             print('----------------------------------------------------')
@@ -199,7 +199,7 @@ def scanner_targetted(CIDR):
             telnet.write('%s\n' % host)
 
     print("[+] Scanning for Mongo Dababases...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p27017,27018,27019,28017 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p27017,27018,27019,28017 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/mongo', 'a') as mongo:
             print('----------------------------------------------------')
@@ -207,7 +207,7 @@ def scanner_targetted(CIDR):
             mongo.write('%s\n' % host)
 
     print("[+] Scanning for VNC...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p5900 --open')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p5900 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/vnc', 'a') as vnc:
             print('----------------------------------------------------')
@@ -215,7 +215,7 @@ def scanner_targetted(CIDR):
             vnc.write('%s\n' % host)
 
     print("[+] Scanning for DNS...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p53 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p53 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/dns', 'a') as dns:
             print('----------------------------------------------------')
@@ -223,7 +223,7 @@ def scanner_targetted(CIDR):
             dns.write('%s\n' % host)
 
     print("[+] Scanning for PHPMyAdmin...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p8089 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p8089 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/phpmyadmin', 'a') as phpmyadmin:
             print('----------------------------------------------------')
@@ -231,7 +231,7 @@ def scanner_targetted(CIDR):
             phpmyadmin.write('%s\n' % host)
 
     print("[+] Scanning for TightVNC...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p5800 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p5800 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/tightvnc', 'a') as tightvnc:
             print('----------------------------------------------------')
@@ -239,7 +239,7 @@ def scanner_targetted(CIDR):
             tightvnc.write('%s\n' % host)
 
     print("[+] Scanning for IBM WebSphere...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p9443 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p9443 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/websphere', 'a') as websphere:
             print('----------------------------------------------------')
@@ -248,7 +248,7 @@ def scanner_targetted(CIDR):
             websphere.write('%s\n' % host)
 
     print("[+] Scanning for Firebird Databases...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p3050 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p3050 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/firebird', 'a') as firebird:
             print('----------------------------------------------------')
@@ -257,7 +257,7 @@ def scanner_targetted(CIDR):
             firebird.write('%s\n' % host)
 
     print("[+] Scanning for XServer...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p6000 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p6000 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/xserver', 'a') as xserver:
             print('----------------------------------------------------')
@@ -265,7 +265,7 @@ def scanner_targetted(CIDR):
             xserver.write('%s\n' % host)
 
     print("[+] Scanning for SNV Repositories...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p3690 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p3690 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/svn', 'a') as svn:
             print('----------------------------------------------------')
@@ -273,7 +273,7 @@ def scanner_targetted(CIDR):
             svn.write('%s\n' % host)
 
     print("[+] Scanning for SNMP...")
-    nm.scan(hosts=CIDR, arguments='-T4 -Pn -p161 --open')
+    nm.scan(hosts=cidr, arguments='-T4 -Pn -p161 --open')
     for host in nm.all_hosts():
         with open('/home/pi/WarBerry/Results/snmp', 'a') as snmp:
             print('----------------------------------------------------')
@@ -281,39 +281,40 @@ def scanner_targetted(CIDR):
             snmp.write('%s\n' % host)
 
 
-def scanner_full(CIDR):
+def scanner_full(cidr):
     print(bcolors.OKGREEN + "      [ TCP/UDP NETWORK SCANNER MODULE ]\n" + bcolors.ENDC)
 
     if os.path.isfile('/home/pi/WarBerry/Results/tcp_udp_scan'):
         print(bcolors.WARNING + "[!] TCP/UDP Results File Exists. Previous Results will be Overwritten " + bcolors.ENDC)
 
-    print("Beginning Scanning Subnet %s" % CIDR)
+    print("Beginning Scanning Subnet %s" % cidr)
 
     print(" ")
     nm = nmap.PortScanner()
 
     print("[+] Scanning TCP/UDP Ports in all hosts...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -sT -sU --open -o /home/pi/WarBerry/Results/tcp_udp_scan')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -sT -sU --open -o /home/pi/WarBerry/Results/tcp_udp_scan')
     for _ in nm.all_hosts():
         print('----------------------------------------------------')
 
 
-def scanner_tcp_full(CIDR):
+def scanner_tcp_full(cidr):
     print(bcolors.OKGREEN + "      [ FULL TCP NETWORK SCANNER MODULE ]\n" + bcolors.ENDC)
 
     if os.path.isfile('/home/pi/WarBerry/Results/tcp_full'):
         print(bcolors.WARNING + "[!] Full TCP Results File Exists. Previous Results will be Overwritten" + bcolors.ENDC)
 
-    print("Beginning Scanning Subnet %s" % CIDR)
+    print("Beginning Scanning Subnet %s" % cidr)
 
     print(" ")
     nm = nmap.PortScanner()
 
     print("[+] Scanning All TCP Ports in all hosts...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 -p1-65535 --open -o /home/pi/WarBerry/Results/tcp_full')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 -p1-65535 --open -o /home/pi/WarBerry/Results/tcp_full')
     for host in nm.all_hosts():
         print('----------------------------------------------------')
         print(host + nm[host].hostname() + " open TCP ports: ")
+        lport = None
         for proto in nm[host].all_protocols():
             lport = nm[host][proto].keys()
             lport.sort()
@@ -321,21 +322,22 @@ def scanner_tcp_full(CIDR):
             print(bcolors.OKGREEN + ' [+] ' + bcolors.ENDC + '%s' % port)
 
 
-def scanner_top(CIDR):
+def scanner_top(cidr):
     print(bcolors.OKGREEN + "      [ TOP TCP PORTS NETWORK SCANNER MODULE ]\n" + bcolors.ENDC)
 
     if os.path.isfile('/home/pi/WarBerry/Results/top_tcp'):
         print(bcolors.WARNING + "[!] Top TCP Ports Results File Exists. Previous Results will be Overwritten" + bcolors.ENDC)
 
-    print("Beginning Scanning Subnet %s" % CIDR)
+    print("Beginning Scanning Subnet %s" % cidr)
     print(" ")
     nm = nmap.PortScanner()
 
     print("[+] Scanning TOP 1000 TCP Ports in all hosts...")
-    nm.scan(hosts=CIDR, arguments='-Pn -T4 --top-ports 1000 --open -o /home/pi/WarBerry/Results/tcp_top')
+    nm.scan(hosts=cidr, arguments='-Pn -T4 --top-ports 1000 --open -o /home/pi/WarBerry/Results/tcp_top')
     for host in nm.all_hosts():
         print('----------------------------------------------------')
         print(host + nm[host].hostname() + " open TCP ports: ")
+        lport = None
         for proto in nm[host].all_protocols():
             lport = nm[host][proto].keys()
             lport.sort()
