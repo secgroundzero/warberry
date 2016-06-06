56eddd61bbfb84d2d1a84ec0014b330cf42b658d
SecGroundZero <secgroundzero@gmail.com>
2016-06-06 06:31:25 +0000

    optparse and bluetooth scans

--- warberry.py
+++ warberry.py
@@ -25,7 +25,7 @@ import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
 
-
+from optparse import OptionParser
 import subprocess
 import os, os.path
 import sys, getopt
@@ -45,6 +45,7 @@ from urllib import urlopen
 import ftplib
 import time
 from netaddr import *
+from bluetooth import *
 #External modules
 from info_banners import *
 from network_scanners import *
@@ -52,143 +53,163 @@ from services_enum import *
 from rest_bypass import *
 from console_colors import bcolors
 from network_thread_scanner import *
-#from service_thread_Enumeration import *
 
-def main(argv):
-        if argv == '-h' or argv == '--help':
-            subprocess.call('clear', shell = True)
-            banner_full()
-        elif argv == '-m' or argv == '--man':
-            subprocess.call('clear', shell = True)
-            banner_full_help()
-        elif argv== '-A' or argv == '--attack':
-            subprocess.call('clear', shell = True)
-            banner()
-            if not os.geteuid() == 0:
-                print bcolors.FAIL + '*** You are not running as root and some modules may fail ***. Run again with sudo. \n' + bcolors.ENDC
-            dhcp_check()
-            if 'eth0' in open('/proc/net/dev').read():
-                iface = 'eth0'
-            else:
-                iface = 'eth1'
-            int_ip = iprecon(iface)
-            if (int_ip==None):
-                exit
-            else:
-                netmask = netmask_recon(iface)
-                external_IP_recon()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed IP Recon\n")
-                sniffer(iface)
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed sniffing network packets\n")
-                pcap_parser()
-                CIDR = subnet(int_ip, netmask)
-                hostnames(CIDR)
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed hostnames search\n")
-                nbtscan(CIDR)
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed NBTScan\n")
+
+def main():
+
+    version = bcolors.TITLE + ( '''
+ _    _  ___  ____________ ___________________   __
+| |  | |/ _ \ | ___ \ ___ \  ___| ___ \ ___ \ \ / /
+| |  | / /_\ \| |_/ / |_/ / |__ | |_/ / |_/ /\ V /
+| |/\| |  _  ||    /| ___ \  __||    /|    /  \ /
+\  /\  / | | || |\ \| |_/ / |___| |\ \| |\ \  | |
+ \/  \/\_| |_/\_| \_\____/\____/\_| \_\_| \_| \_/
+
+            TACTICAL EXPLOITATION
+
+v2.0                              @sec_groundzero
+''') + bcolors.ENDC
+
+
+    parser = OptionParser(usage= "usage: sudo %prog [options]",version=version)
+    parser.add_option("-a", "--attack", action="store", dest="attacktype", default="-A", help="Attack Mode."+ bcolors.WARNING + " Default: --attack" + bcolors.ENDC)
+    parser.add_option("-p", "--packets", action="store", dest="packets", default=20, type=int, help="Number of Network Packets to capture")
+    parser.add_option("-i", "--interface", action="store", dest="iface", default="eth0",help="Network Interface to use." + bcolors.WARNING + " Default: eth0" + bcolors.ENDC, choices=['eth0', 'eth1', 'wlan0', 'wlan1', 'wlan2', 'at0'])
+    parser.add_option("-P", "--poison", action="store_false",dest="poison",default=True, help="Turn Poisoning on/off."+ bcolors.WARNING + " Default: On" + bcolors.ENDC)
+    parser.add_option("-H", "--hostname", action="store_false", dest="hostname", default= True, help="Change WarBerry hostname" + bcolors.WARNING + " Default: On" + bcolors.ENDC)
+    parser.add_option("-e", "--enumeration", action="store_true",dest="enum", default=False, help="Turn enumeration mode on/off." + bcolors.WARNING + " Default: On" + bcolors.ENDC)
+    parser.add_option("-r", "--recon", action="store_true", dest="reconmode", default=False,help="Recon only mode. No port scans")
+    parser.add_option("-S", "--sniffer", action="store_true", dest="sniffer", default=False,help="Sniffer only mode.")
+    parser.add_option("-C", "--clear", action="store_true", dest="clear", default=False, help="Clear previous output folders in ../Results")
+    parser.add_option("-m", "--man", action="store_true", dest="manpage", default=False, help="Print WarBerry man pages")
+
+
+    (options, args) = parser.parse_args()
+
+
+
+    if options.clear == True:
+        clear_output()
+    elif options.manpage == True:
+        subprocess.call('clear', shell=True)
+        banner_full_help()
+    elif options.attacktype == '-A' or options.attacktype == '--attack':
+        subprocess.call('clear', shell=True)
+        banner()
+        if not os.geteuid() == 0:
+            print bcolors.FAIL + '*** You are not running as root and some modules will fail ***\nRun again with sudo.' + bcolors.ENDC
+            sys.exit(-1)
+        dhcp_check()
+        iface = options.iface
+        int_ip = iprecon(iface)
+        if (int_ip == None):
+            exit
+        else:
+            netmask = netmask_recon(iface)
+            external_IP_recon()
+            with open('../Results/running_status', 'a') as status:
+                status.write("Completed IP Recon\n")
+            packets = options.packets
+            sniffer(iface, packets)
+            with open('../Results/running_status', 'a') as status:
+                status.write("Completed sniffing network packets\n")
+            pcap_parser()
+            CIDR = subnet(int_ip, netmask)
+            hostnames(CIDR)
+            with open('../Results/running_status', 'a') as status:
+                status.write("Completed hostnames search\n")
+            nbtscan(CIDR)
+            with open('../Results/running_status', 'a') as status:
+                status.write("Completed NBTScan\n")
+            if options.hostname == True:
                 namechange()
-                #scanner_targetted(CIDR)
+            if options.reconmode == False:
                 scanner_thread(CIDR)
                 with open('../Results/running_status', 'a') as status:
                     status.write("Completed targeted scanning\n")
-                shares_enum()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed enumerating shares\n")
-                smb_users()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed enumerating users\n")
-                http_title_enum()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed enumerating HTTP Titles\n")
-                nfs_enum()
-                with open('../Results/running_status', 'a') as status:
-                   status.write("Completed NFS Enumeration\n")
-                waf_enum()
-                with open('../Results/running_status', 'a') as status:
-                   status.write("Completed WAF Enumeration\n")
-                mysql_enum()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed MYSQL Enumeration\n")
-                mssql_enum()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed MSSQL Enumeration\n")
-                ftp_enum()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed FTP Enumeration\n")
-                snmp_enum()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed SNMP Enumeration\n")
-                wifi_scan()
-                with open('../Results/running_status', 'a') as status:
-                    status.write("Completed wifi networks scan\n")
+                if options.enum == False:
+                    shares_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed enumerating shares\n")
+                    smb_users()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed enumerating users\n")
+                    webs_prep()
+                    http_title_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed enumerating HTTP Titles\n")
+                    nfs_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed NFS Enumeration\n")
+                    waf_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed WAF Enumeration\n")
+                    mysql_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed MYSQL Enumeration\n")
+                    mssql_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed MSSQL Enumeration\n")
+                    ftp_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed FTP Enumeration\n")
+                    snmp_enum()
+                    with open('../Results/running_status', 'a') as status:
+                        status.write("Completed SNMP Enumeration\n")
+            bluetooth_scan()
+            with open('../Results/running_status', 'a') as status:
+                status.write("Completed bluetooth scan\n")
+            wifi_scan()
+            with open('../Results/running_status', 'a') as status:
+                status.write("Completed wifi networks scan\n")
                 print ""
-                print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
-                print " "
+            print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
+            print " "
+            if options.poison == True:
                 with open('../Results/running_status', 'a') as status:
                     status.write("Entering poisoning mode\n")
-                poison(iface)
+                    poison(iface)
 
-        elif argv == '-T' or argv == '--toptcp':
-            subprocess.call('clear', shell = True)
-            banner()
-            if 'eth0' in open('/proc/net/dev').read():
-                iface = 'eth0'
-            else:
-                iface = 'eth1'
-            int_ip = iprecon(iface)
-	    if (int_ip==None):
-                exit
-            netmask = netmask_recon(iface)
-            print external_IP_recon()
-            CIDR = subnet(int_ip, netmask)
-            scanner_top(CIDR)
-            print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
-        elif argv == '-B' or argv == '--tcpudp':
-            subprocess.call('clear', shell = True)
-            banner()
-            if 'eth0' in open('/proc/net/dev').read():
-                iface = 'eth0'
-            else:
-                iface = 'eth1'
-            int_ip = iprecon(iface)
-	    if (int_ip==None):
-                exit
-            netmask = netmask_recon(iface)
-            print external_IP_recon()
-            CIDR = subnet(int_ip, netmask)
-            scanner_full(CIDR)
-            print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
-        elif argv == '-F' or argv == '--fulltcp':
-            subprocess.call('clear', shell = True)
-            banner()
-            if 'eth0' in open('/proc/net/dev').read():
-                iface = 'eth0'
-            else:
-                iface = 'eth1'
-            int_ip = iprecon(iface)
-            netmask = netmask_recon(iface)
-            print external_IP_recon()
-            CIDR = subnet(int_ip, netmask)
-            scanner_tcp_full(CIDR)
-            print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
-        elif argv == '-S' or argv == '--sniffer':
-            if 'eth0' in open('/proc/net/dev').read():
-                iface = 'eth0'
-            else:
-                iface = 'eth1'
-            subprocess.call('clear', shell = True)
-            sniffer(iface)
-        elif argv == '-C' or argv == '--clear':
-            clear_output()
-        elif argv == ' ':
-            subprocess.call('clear', shell = True)
-            banner()
-        else:
-            print bcolors.WARNING + "[!] Invalid Module Selected. Use -h or --help for the help function\n" + bcolors.ENDC
+    elif options.attacktype == '-T' or options.attacktype == '--toptcp':
+        subprocess.call('clear', shell=True)
+        banner()
+        iface = options.iface
+        int_ip = iprecon(iface)
+        if (int_ip == None):
+            exit
+        netmask = netmask_recon(iface)
+        print external_IP_recon()
+        CIDR = subnet(int_ip, netmask)
+        scanner_top(CIDR)
+        print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
+
+    elif options.attacktype == '-B' or options.attacktype == '--tcpudp':
+        subprocess.call('clear', shell=True)
+        banner()
+        iface = options.iface
+        int_ip = iprecon(iface)
+        if (int_ip == None):
+            exit
+        netmask = netmask_recon(iface)
+        print external_IP_recon()
+        CIDR = subnet(int_ip, netmask)
+        scanner_full(CIDR)
+        print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
+    elif options.attacktype == '-F' or options.attacktype == '--fulltcp':
+        subprocess.call('clear', shell=True)
+        banner()
+        iface = options.iface
+        int_ip = iprecon(iface)
+        netmask = netmask_recon(iface)
+        print external_IP_recon()
+        CIDR = subnet(int_ip, netmask)
+        scanner_tcp_full(CIDR)
+        print bcolors.TITLE + "All scripts completed. Check the /Results directory" + bcolors.ENDC
+    elif options.attacktype == '-S' or options.attacktype == '--sniffer':
+        iface = options.iface
+        packets = options.packets
+        subprocess.call('clear', shell=True)
+        sniffer(iface, packets)
 
 
 def dhcp_check():
@@ -213,7 +234,7 @@ def iprecon(ifname):
         netmask = socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', ifname))[20:24])
 
         if not ip_validate(int_ip):
-            print '[+] Internal IP obtained on %s:' % ifname + bcolors.OKGREEN + " %s" % int_ip + bcolors.ENDC + ' netmask ' + bcolors.OKGREEN + '%s' % netmask + bcolors.ENDC
+            print '[+] Internal IP obtained on '+ bcolors.TITLE + '%s:' % ifname + bcolors.ENDC + bcolors.OKGREEN + " %s" % int_ip + bcolors.ENDC + ' netmask ' + bcolors.OKGREEN + '%s' % netmask + bcolors.ENDC
             return int_ip
 
         else:
@@ -304,7 +325,7 @@ def clear_output():
 
             sys.stdout.write("Please respond with 'y/yes' or 'n/no'\n")
 
-def sniffer(iface):
+def sniffer(iface, packets):
         print " "
         packet_count = 20
         pcap_location = "../Results/capture.pcap"
@@ -312,7 +333,7 @@ def sniffer(iface):
         print "Sniffer will begin capturing %d packets" %packet_count #Change the count number accordingly
         packets = sniff(iface=iface, count= packet_count)
         wrpcap(pcap_location, packets)
-        print bcolors.OKGREEN + "[+] Capture Completed." + bcolors.ENDC + " PCAP File Saved at " + bcolors.OKGREEN + "%s!\n" %pcap_location + bcolors.ENDC
+        print bcolors.OKGREEN + "[+] " + bcolors.ENDC + "Capture Completed." + bcolors.ENDC + " PCAP File Saved at " + bcolors.OKGREEN + "%s!\n" %pcap_location + bcolors.ENDC
 
 
 def wifi_scan():
@@ -326,16 +347,49 @@ def wifi_scan():
                 print bcolors.OKGREEN + "      [ Wi-Fi ENUMERATION MODULE ]\n" + bcolors.ENDC
 
                 subprocess.call("sudo iwlist wlan0 scan | grep ESSID | awk {'print $1'} > ../Results/wifis", shell = True)
-                with open('../Results/wifis', 'r') as wifis:
-                    if os.stat('../Results/wifis').st_size != 0:
-                        for wifi in wifis:
-                            print bcolors.OKGREEN + "[+] Found Wireless Network: %s" %wifi.strip() + bcolors.ENDC
-                    else:
-                        print bcolors.WARNING + "[-] No Wireless Networks Captured" + bcolors.ENDC
-
+                if os.path.isfile('../Results/wifis'):
+                    with open('../Results/wifis', 'r') as wifis:
+                        if os.stat('../Results/wifis').st_size != 0:
+                            for wifi in wifis:
+                                print bcolors.OKGREEN + "[+] " + bcolors.ENDC + "Found Wireless Network: %s" %wifi.strip() + bcolors.ENDC
+                        else:
+                            print bcolors.WARNING + "[-] No Wireless Networks Captured" + bcolors.ENDC
+                else:
+                    return
             else:
                 return
 
+
+def bluetooth_scan():
+
+    subprocess.call("sudo cat /proc/cpuinfo | grep Revision | awk {'print $3'} > ../Results/model", shell=True)
+
+    with open('../Results/model', 'r') as pi_model:
+        for model in pi_model:
+            if model.strip() == "a02082":
+                print " "
+                print bcolors.OKGREEN + "      [ BLUETOOTH ENUMERATION MODULE ]\n" + bcolors.ENDC
+
+                def discover():
+                    blues = discover_devices()
+                    for device in blues:
+                        name = str(lookup_name(device))
+                        with open('../Results/blues','a') as bluesfile:
+                            bluesfile.write(str(name) + " " + str(device)+ "\n")
+
+                        print bcolors.OKGREEN + "[+]" + bcolors.ENDC + " Found Bluetooth Device: " + str(name)
+                        print bcolors.OKGREEN + "[+]" + bcolors.ENDC + " MAC address: " + str(device)
+
+                for i in range(10):
+                    discover()
+            else:
+                return
+    if os.path.isfile('../Results/blues'):
+        if os.stat('../Results/blues').st_size != 0:
+            print bcolors.TITLE + "[+] Done! Results saved in /Results/blues" + bcolors.ENDC
+    else:
+        print bcolors.WARNING + "[-] No Bluetooth Devices Captured" + bcolors.ENDC
+    
 
 def nbtscan(CIDR):
 
@@ -354,7 +408,7 @@ def nbtscan(CIDR):
 if __name__ == '__main__':
 
     try:
-        main(sys.argv[1])
+        main()
     except KeyboardInterrupt:
         subprocess.call("sudo mkdir ../Results/Responder_logs", shell=True)
         subprocess.call("sudo mv ../Tools/Responder/logs/* ../Results/Responder_logs/",shell=True)
