ab2daac42c4e69f87d165425c4aeff6ee4402ead
SecGroundZero <secgroundzero@gmail.com>
2016-09-09 20:36:35 +0000

    changed version

--- src/utils/info_banners.py
+++ src/utils/info_banners.py
@@ -38,7 +38,7 @@ def banner():
 
             TACTICAL EXPLOITATION
 
-v4.0a                              @sec_groundzero
+v4.0b                              @sec_groundzero
                            secgroundzero@gmail.com
 ''') + bcolors.ENDC
 
