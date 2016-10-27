4312b38d1117ea02eb5bdb9f64f3a3307c58016f
SecGroundZero <secgroundzero@gmail.com>
2016-10-27 11:30:46 +0000

    updated banner

--- src/utils/info_banners.py
+++ src/utils/info_banners.py
@@ -38,7 +38,7 @@ def banner():
 
             TACTICAL EXPLOITATION
 
-v4.c1                              @sec_groundzero
+v4.c1a                             @sec_groundzero
                            secgroundzero@gmail.com
 ''') + bcolors.ENDC
 
