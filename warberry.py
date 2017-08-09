1b8020c32e00eeb0dcfc5881161aad34eb3cdf57
SecGroundZero <secgroundzero@gmail.com>
2017-08-09 19:26:12 +0000

    eth0 is the default iface

--- warberry.py
+++ warberry.py
@@ -6,7 +6,6 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -29,6 +28,7 @@ def main():
 
     war_arg_list=[]
     responder_time = 1000
+    iface="eth0"
     for i in range(1,len(sys.argv)):
         if sys.argv[i]=="-t" or sys.argv[i]=="-time":
             responder_time= sys.argv[i+1]
