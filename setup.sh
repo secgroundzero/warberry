903493eddad7d97818388e3f8feec4ceb3457863
Tango43 <olivier.gireaudot@gmail.com>
2017-09-26 09:21:39 +0200

    Update setup.sh
    Added urllib3

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ apt-get install -y nbtscan
 apt-get install -y python-scapy
 apt-get install -y tcpdump
 apt-get install -y nmap
+apt-get install -y python-urllib3
 pip install python-nmap
 apt-get install -y python-bluez
 pip install optparse-pretty
